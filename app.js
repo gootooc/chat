f081c82664d05f8c3bab28b0864611c232c846b7
gootooc <gootooc@gmail.com>
2020-12-19 18:03:42 +0900

    complete ver 1.0

--- app.js
+++ app.js
@@ -6,24 +6,36 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const socketIO = require("socket.io");
+const moment = require("moment");
+
 
 const io = socketIO(server); // 채팅 메시지를 받아온다.
 //import * as socketIO from "./stocket.io"
 
-app.use(express.static(path.join(__dirname, "src")))
+app.use(express.static(path.join(__dirname, "src")));
 
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT, () => console.log(`server is running ${PORT}`))
-//app.listen(PORT, () => console.log(`server is running ${PORT}`)) // 위 구문으로 변경한다.
+const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
     socket.on("chatting", (data) =>{
         console.log(data);
-        io.emit("chatting", `chat server ${data}`)
+        const { name, msg } = data;
+        io.emit("chatting", {
+            // name: name,
+            // msg: msg,
+            name,
+            msg,
+            time: moment(new Date()).format("h:ss A"),
+        });
     });
-
-    //console.log("socket is connected");
 })
 
+
+server.listen(PORT, () => console.log(`server is running ${PORT}`));
+//app.listen(PORT, () => console.log(`server is running ${PORT}`)) // 위 구문으로 변경한다.
+
+
 // npm install -g nodemon
+
+// ngrok 로 호스팅 할 수 있다.
+//npm install -g ngrok
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,11 +1,68 @@
 'use strict'
 const socket = io();
 
-socket.emit("chatting", "form  front");
+const nickname = document.querySelector("#nickname");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector('.send-button');
+const chatList = document.querySelector('.chatting-list');
+const displayContainer = document.querySelector('.display-container');
+
+
+const send = () => {
+    const param = {
+        name: nickname.value,
+        msg: chatInput.value
+    }
+
+    socket.emit("chatting", param);
+}
+
+
+sendButton.addEventListener("click", send );
+
+chatInput.addEventListener('keypress',(evt) =>{
+    if(evt.keyCode === 13){
+        send();
+        
+    }
+})
+
+
+
+//socket.emit("chatting", "form  front");
 
 socket.on("chatting", (data) =>{
-    console.log(data);
+    const { name, msg, time } = data;
+    const item = new LIModel(name, msg, time);
+    item.makeLi();
+    displayContainer.scrollTo(0, displayContainer.scrollHeight);
+    // const li = document.createElement("li");
+    // li.innerText = `${data.name} 님이 - ${data.msg}`;
+    // chatList.appendChild(li);
+    // console.log(data);
 });
 
 
-console.log(socket);
+function LIModel(name, msg, time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received");
+        const dom = ` <span class="profile">
+        <span class="user">${this.name}</span>
+        <img class="image" src="https:placeimg.com/50/50/any" alt="icon" />
+        </span>
+        <span class="message">${this.msg}</span>
+        <span class="time">${this.time}</span>`;
+
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+
+        chatInput.value = "";
+        chatInput.focus();
+        
+    }
+}
